2d4a92ba63568da1437d17074d97b88cee639e20
AlexandreCote <acoteomg@gmail.com>
2018-09-14 19:16:52 -0400

    [Lecture 45] Adding Sound

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -5,10 +5,13 @@ using UnityEngine;
 public class Rocket : MonoBehaviour {
 
     Rigidbody rigidBody;
+    AudioSource rocketSound;
+    float fullRotationTime = 2f;
 
 	// Use this for initialization
 	void Start () {
         rigidBody = GetComponent<Rigidbody>();
+        rocketSound = GetComponent<AudioSource>();
 	}
 	
 	// Update is called once per frame
@@ -18,17 +21,34 @@ public class Rocket : MonoBehaviour {
 
     private void ProcessInput()
     {
+
+        //Defines thrust behavior
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rocketSound.Play();
+        }
+
         if (Input.GetKey(KeyCode.Space))
         {
             rigidBody.AddRelativeForce(Vector3.up);
         }
+
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            rocketSound.Stop();
+        }
+
+        //Defines roation behavior
         if (Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.D))
         {
-            print("Left!");
+            float rotationAngle = (360f / fullRotationTime) * Time.deltaTime;
+            transform.Rotate(new Vector3(0, 0, rotationAngle));
         }
         if (Input.GetKey(KeyCode.D) && !Input.GetKey(KeyCode.A))
         {
-            print("Right!");
+            float rotationAngle = (-360f / fullRotationTime) * Time.deltaTime;
+            transform.Rotate(new Vector3(0, 0, rotationAngle));
         }
+        //-------------------------------
     }
 }
