e470db61403e874c9f75d8a06e2c1319ce9c9a44
AlexandreCote <acoteomg@gmail.com>
2018-09-15 13:20:31 -0400

    Level 1 Done

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -6,6 +6,8 @@ public class Rocket : MonoBehaviour {
 
     Rigidbody rigidBody;
     AudioSource rocketSound;
+    bool dead = false;
+    Vector3 startPosition;
 
     //Defines the time it should take to make a full 360 degree rotation.
     [SerializeField] float fullRotationTime = 2f;
@@ -20,18 +22,47 @@ public class Rocket : MonoBehaviour {
         rocketSound.Stop();         //Fixes annoying crackling bug
 
         thrusterStrength = rigidBody.mass*thrusterMultiplier;
+        startPosition = transform.position;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        thrusterStrength = rigidBody.mass * thrusterMultiplier;
-        Rotate();
-        Thrust();
+        if (!dead)
+        {
+            Rotate();
+            Thrust();
+        }
+        else
+        {
+            Reset();
+        }
+
+
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        switch (collision.gameObject.tag)
+        {
+            case "Friendly":
+                //No effect
+                break;
+
+            case "Goal":
+                //win level
+                break;
+
+            default:
+                Death(collision);
+                break;
+        }   
     }
 
     private void Rotate()
     {
         //Defines rotation behavior
+
+
         float rotationPerFrame = 360f / fullRotationTime * Time.deltaTime;
 
         if (Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.D))
@@ -60,17 +91,12 @@ public class Rocket : MonoBehaviour {
         if (Input.GetKey(KeyCode.Space))
         {
             rigidBody.freezeRotation = true;            //Takes manual control of rotation
+            thrusterStrength = rigidBody.mass * thrusterMultiplier;
+
 
             rigidBody.AddRelativeForce(Vector3.up * thrusterStrength);
 
             rigidBody.freezeRotation = false;           //Resumes physics engine control of rotation
-        }
-        else if (Input.GetKey(KeyCode.S))
-        {
-            if (rigidBody.velocity.y > 0)
-            {
-                rigidBody.AddRelativeForce(Vector3.up * (-rigidBody.velocity.y * thrusterStrength)/20f);
-            }
         }
 
         //end rocket sound on key up
@@ -81,4 +107,38 @@ public class Rocket : MonoBehaviour {
 
 
     }
+
+    void Death(Collision collision)
+    {
+        dead = true;
+        rocketSound.Stop();             //Prevents sound continuing to play when dying while holding space.
+
+        Collider[] colliders = GetComponentsInChildren<Collider>();     //Disables collision
+
+        foreach (Collider collider in colliders)
+        {
+            collider.enabled = false;
+        }
+    }
+
+    private void Reset()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            dead = false;
+
+            Collider[] colliders = GetComponentsInChildren<Collider>();
+
+            foreach (Collider collider in colliders)
+            {
+                collider.enabled = true;
+            }
+
+            rigidBody.freezeRotation = true;
+            rigidBody.velocity = Vector3.zero;
+            transform.SetPositionAndRotation(startPosition, new Quaternion(0, 0, 0, 0));
+            rigidBody.freezeRotation = false;
+        }
+    }
+
 }
